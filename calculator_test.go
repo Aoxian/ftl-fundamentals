9acda159c31b19fb24b0a7a6d2c5052199f53690
iang <iang@IanGs-MacBook-Pro.local>
2020-09-06 14:41:08 -0700

    Fix Subtract method
    - Subtract now works as expected
    - Add Arithmetics test hierary

--- calculator_test.go
+++ calculator_test.go
@@ -5,20 +5,22 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
-	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
-	}
-}
+func TestArithmetics(t *testing.T) {
+	t.Run("TestAdd", func(t *testing.T) {
+		t.Parallel()
+		var want float64 = 4
+		got := calculator.Add(2, 2)
+		if want != got {
+			t.Errorf("want %f, got %f", want, got)
+		}
+	})
 
-// func TestSubtract(t *testing.T) {
-// 	t.Parallel()
-// 	var want float64 = 2
-// 	got := calculator.Subtract(4, 2)
-// 	if want != got {
-// 		t.Errorf("want %f, got %f", want, got)
-// 	}
-// }
+	t.Run("TestSubtract", func(t *testing.T) {
+		t.Parallel()
+		var want float64 = 2
+		got := calculator.Subtract(4, 2)
+		if want != got {
+			t.Errorf("want %f, got %f", want, got)
+		}
+	})
+}
